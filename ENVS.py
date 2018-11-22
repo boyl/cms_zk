97b450430f2f6d1746540d9be49742f71cceccdc
boyl <1228417956@qq.com>
2018-11-22 10:07:12 +0800

    modify

---
+++ ENVS.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+DICT_TOKEN_PARAM = {
+    "qywx": {
+        "token_url": "https://qyapi.weixin.qq.com/cgi-bin/service/get_provider_token",
+        "token_data": {
+            "corpid": "wwb712a415cce67382",
+            "provider_secret": "PiAHUR71Vo0F8axiJiQFlgLJ__KEnhPFiyrowWDmw4f1ZVWXoptUUuCpqJ_Ur5_Q"
+        }
+    },
+    "fufu": {
+        "token_url": "http://120.76.167.138:7080//oauth/token?client_id=seade&client_secret=seade&grant_type=client_credentials",
+        "token_data": {}
+    }
+}
